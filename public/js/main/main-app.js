e550980515634a13260a4547f3d31433caddbc08
ofvvv <mrsabrozongo@gmail.com>
2026-03-17 10:18:26 +0000

    archivos re organizados

--- public/js/main/main-app.js
+++ public/js/main/main-app.js
@@ -410,11 +410,11 @@ function speakMsg(d){
 
     if (cfg.ttsNoEmoji !== false) {
         // Regex para eliminar emojis, incluidos los que tienen variaciones de tono de piel y género
-        textToSpeak = textToSpeak.replace(/[\\u{1F600}-\\u{1F64F}]|[\\u{1F300}-\\u{1F5FF}]|[\\u{1F680}-\\u{1F6FF}]|[\\u{1F1E0}-\\u{1F1FF}]|[\\u{2600}-\\u{27BF}]|[\\u{1F900}-\\u{1F9FF}]|[\\u{1F004}-\\u{1F0CF}]/gu, '');
+        textToSpeak = textToSpeak.replace(/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{27BF}]|[\u{1F900}-\u{1F9FF}]|[\u{1F004}-\u{1F0CF}]/gu, '');
         // Eliminar también los emotes de Twitch/7TV por su nombre
         if (d.plat === 'TW') {
             Object.keys(emoteMap).forEach(emoteName => {
-                textToSpeak = textToSpeak.replace(new RegExp(`\\b${emoteName}\\b`, 'g'), '');
+                textToSpeak = textToSpeak.replace(new RegExp(`\b${emoteName}\b`, 'g'), '');
             });
         }
     }
