59b60953a808311b0be88214271df85ee4d2f2f1
ofvvv <mrsabrozongo@gmail.com>
2026-03-17 16:53:29 +0000

    rework befor

--- public/js/main/globals.js
+++ public/js/main/globals.js
@@ -4,3 +4,7 @@ let totalMsgCount = 0;
 
 // Estado global de la configuracion (se rellena al inicio)
 let cfg = {};
+
+// La URL del servidor y el socket se definen aquí para que sean globales
+const SERVER = 'http://localhost:3000';
+const socket = io(SERVER);
